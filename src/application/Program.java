444443880dad64c68e989c9245217de11cf79d81
rbarbosa1985 <betoanselmo@gmail.com>
2022-01-24 21:48:04 -0300

    Special Move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,6 +40,12 @@ public class Program {
 					captured.add(capturedPiece);
 				}
 				
+				if(chessMatch.getPromoted() != null) {
+					System.out.print("Escolha a peça que deseja ( B / N / R / Q ): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+				
 			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
